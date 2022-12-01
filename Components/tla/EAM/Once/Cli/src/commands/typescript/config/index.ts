edb85ac718a13b21ec7f059f3e2fba143e54aa3e
phibar | CeruleanCircle <philipp@ceruleancircle.com>
2022-12-01 23:30:29 +0100

    Added eslint config

--- Components/tla/EAM/Once/Cli/src/commands/typescript/config/index.ts
+++ Components/tla/EAM/Once/Cli/src/commands/typescript/config/index.ts
@@ -13,7 +13,7 @@ export default class TypescriptConfig extends Command {
   	// flag with no value (-f, --force)
   	force: Flags.boolean({char: 'f'}),
   }
-
+  
   static args = [{name: 'file'}]
 
   public async run(): Promise<void> {
