88f3c9d26eb6a569ce3419e2af39a817b489f0a1
phibar <philipp@ceruleancircle.com>
2022-12-02 13:23:15 +0100

    feat: Included to Monorepo (#3)
    * added pnpm and oclif
    
    * removed oclif
    
    * added working oclif
    
    * update depedencies
    
    * updated deps
    
    * test
    
    * Added Pnpm component
    
    * moved files
    
    * moved to esm
    
    * cleanup
    
    * added License
    
    * Added Turbo
    
    * fix
    
    * Fixed Pipe
    
    * cleanup
    
    * refactored eslint
    
    * removed foo
    
    * delete workspace eslintrc

--- Components/tla/EAM/Once/Cli/src/commands/hello/once.ts
+++ Components/tla/EAM/Once/Cli/src/commands/hello/once.ts
@@ -1,19 +1,19 @@
 import {Command} from '@oclif/core'
 
 export default class Once extends Command {
-  static description = 'Say hello once'
+	static description = 'Say hello once'
 
-  static examples = [
-    `<%= config.bin %> <%= command.id %>
+	static examples = [
+		`<%= config.bin %> <%= command.id %>
 hello world! (./src/commands/hello/world.ts)
 `,
-  ]
+	]
 
-  static flags = {}
+	static flags = {}
 
-  static args = []
+	static args = []
 
-  async run(): Promise<void> {
-    this.log('hello once! (./src/commands/hello/once.ts)')
-  }
+	async run(): Promise<void> {
+		this.log('hello once! (./src/commands/hello/once.ts)')
+	}
 }
