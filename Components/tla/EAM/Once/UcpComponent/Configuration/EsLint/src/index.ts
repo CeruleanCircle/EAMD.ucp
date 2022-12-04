2373f5f5169e9c52565c3dee351816382f9f52f9
phibar | CeruleanCircle <philipp@ceruleancircle.com>
2022-12-04 08:47:14 +0100

    extracted oclif config

--- Components/tla/EAM/Once/UcpComponent/Configuration/EsLint/src/index.ts
+++ Components/tla/EAM/Once/UcpComponent/Configuration/EsLint/src/index.ts
@@ -6,5 +6,6 @@ export = {
 		base,
 		browser: { ...base, env: { es2022: true, browser: true } },
 		node: { ...base, env: { es2022: true, node: true } },
+		oclif: { ...base, extends:[...base.extends, 'oclif-typescript'], env: { es2022: true, node: true } },
 	},
 }
